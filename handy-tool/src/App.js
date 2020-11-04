b5b9d1eb3a529bb327e398e8bddccc2ecd4cb2a0
LarrySimiyu <larry.simiyu@gmail.com>
2020-11-03 16:49:42 -0800

    detect hand function

--- handy-tool/src/App.js
+++ handy-tool/src/App.js
@@ -7,10 +7,57 @@ import * as handpose from "@tensorflow-models/handpose"
 import Webcam from "react-webcam";
 
 function App() {
+
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  const runHandPose = async () => {
+    const net = await handpose.load();
+    console.log("Handpose model successfully loaded")
+
+    // loop and detect hands
+  }
+
+  const detect = async (net) => {
+
+  }
+
+  runHandPose();
+
   return (
     <div className="App">
       <header className="App-header">
+        <Webcam 
+          ref={webcamRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 9,
+            width: 640,
+            height: 480,
+          }}
+        />
+
+        <canvas
+            ref={canvasRef}
+            style={{
+              position: "absolute",
+              marginLeft: "auto",
+              marginRight: "auto",
+              left: 0,
+              right: 0,
+              textAlign: "center",
+              zindex: 9,
+              width: 640,
+              height: 480,
+            }}
         
+        />
+
       </header>
     </div>
   );
