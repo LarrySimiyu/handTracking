7f0fd58815815214d1deb54a5873f40cb8bd11a2
LarrySimiyu <larry.simiyu@gmail.com>
2020-11-03 16:12:51 -0800

    imports made

---
+++ handy-tool/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+import React, {useRef} from 'react';
+
+import * as tf from "@tensorflow/tfjs"
+import * as handpose from "@tensorflow-models/handpose"
+import Webcam from "react-webcam";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
