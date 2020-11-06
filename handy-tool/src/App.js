21cd03f96ddcd074377fac830c6010ee698e2f25
LarrySimiyu <larry.simiyu@gmail.com>
2020-11-06 11:32:00 -0800

    some change i missed

--- handy-tool/src/App.js
+++ handy-tool/src/App.js
@@ -1,26 +1,25 @@
-import './App.css';
+import "./App.css";
 
-import React, {useRef} from 'react';
+import React, { useRef } from "react";
 
-import * as tf from "@tensorflow/tfjs"
-import * as handpose from "@tensorflow-models/handpose"
+import * as tf from "@tensorflow/tfjs";
+import * as handpose from "@tensorflow-models/handpose";
 import Webcam from "react-webcam";
-import {drawHand} from "./utilities";
+import { drawHand } from "./utilities";
 
 function App() {
-
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
 
   const runHandPose = async () => {
     const net = await handpose.load();
-    console.log("Handpose model successfully loaded")
+    console.log("Handpose model successfully loaded");
 
     // loop and detect hands
     setInterval(() => {
       detect(net);
     }, 100);
-  }
+  };
 
   const detect = async (net) => {
     // check if data is Available
@@ -43,37 +42,88 @@ function App() {
       canvasRef.current.width = videoWidth;
       canvasRef.current.height = videoHeight;
 
-      // make detections 
+      // make detections
       // grab neural network then estimate haßnd within the video frame
       const hand = await net.estimateHands(video);
       console.log(hand);
 
       // Draw mesh
-      const ctx = canvasRef.current.getContext('2d');
-      drawHand(hand, ctx)
-
+      const ctx = canvasRef.current.getContext("2d");
+      drawHand(hand, ctx);
     }
-  }
+  };
 
-  runHandPose();
+  // runHandPose();
 
-  // TODO: 
-    //TIMER BUTTON - ENTER STUDY LENGTH
-      // START STOP
-      // Random Amount of Motivation Tips
-    
-    // Hand Detection Timer - Triggered By Hand - Seconds to Minutes
-      // Warning Time (user input)
-      // Play warning audio
-      
+  // TODO:
+  //TIMER BUTTON - ENTER STUDY LENGTH
+  // START STOP
+  // Random Amount of Motivation Tips
 
-    
+  // Hand Detection Timer - Triggered By Hand - Seconds to Minutes
+  // Warning Time (user input)
+  // Play warning audio
 
+  let isClockRunning = false;
+
+  let workSessionDuration = 1500;
+  let currentTimeLeftInSession = 1500;
+  let breakSessionDuration = 300;
+  let pomodoroTimer = document.querySelector('#pomodoroTimer');
+
+  const startTimer = () => {
+    toggleClock();
+  };
+
+  const pauseTimer = () => {
+    toggleClock();
+  };
+
+  const stopTimer = () => {
+    toggleClock(true);
+  };
+
+  const toggleClock = (reset) => {
+    if (reset) {
+      //STOP THE TIMER
+    } else if (isClockRunning === true) {
+      // PAUSE THE Timer
+      // clearInterval(clockTimer);
+      isClockRunning = false;
+    } else {
+      //START THE Timer
+      isClockRunning = true;
+
+      const clockTimer = setInterval(() => {
+        // decrease time left / increase time spent
+        currentTimeLeftInSession--;
+        displayCurrentTimeLeftInSession();
+      }, 1000);
+    }
+  };
+  const displayCurrentTimeLeftInSession = () => {
+    const secondsLeft = currentTimeLeftInSession;
+    let result = ''
+    const seconds = secondsLeft % 60;
+    const minutes = parseInt(secondsLeft / 60) % 60;
+    let hours = parseInt(secondsLeft / 3600)
+    // add leading zeroes if its less than 10
+
+    function addLeadingZeroes(time) {
+      return time < 10 ? `0${time}` : time
+    }
+    if (hours > 0) result += `{hours}:`
+    result += `${addLeadingZeroes(minutes)}:{addLeadingZeroes(seconds)}`
+    pomodoroTimer.innerText = result.toString();
+
+  };
+
+  let timer = "poop";
   return (
     <div className="App">
       <h1>WOW THIS IS TOXIC</h1>
       <header className="App-header">
-        <Webcam 
+        <Webcam
           ref={webcamRef}
           style={{
             position: "absolute",
@@ -89,22 +139,26 @@ function App() {
         />
 
         <canvas
-            ref={canvasRef}
-            style={{
-              position: "absolute",
-              marginLeft: "auto",
-              marginRight: "auto",
-              left: 0,
-              right: 0,
-              textAlign: "center",
-              zindex: 9,
-              width: 640,
-              height: 480,
-            }}
-        
+          ref={canvasRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 9,
+            width: 640,
+            height: 480,
+          }}
         />
-
       </header>
+          <div className="pomodoroTimer"> <h1>{timer}</h1></div>
+      
+
+      <button onClick={startTimer}>Start</button>
+      <button onClick={pauseTimer}>Pause</button>
+      <button onClick={stopTimer}>Stop</button>
     </div>
   );
 }
